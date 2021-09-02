a6378121ab0b57b05a37d1d9accc7ca14e80bc50
developbiao <developbiao@gmail.com>
2021-09-02 23:22:08 +0800

    add about page

--- main.go
+++ main.go
@@ -6,7 +6,16 @@ import (
 )
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, this is my goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, here is my goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "<h1>This blog just for record leanring golang, if you have any quetion please contact "+
+			"<a href=\"mailto:developbiao@gmail.com\">developbiao@gmail.com</a></h1>")
+	} else {
+		fmt.Fprint(w, "<h1>Reqeust not found page :(</h1>"+
+			"<p>If you have any doubts, please contact us. </p>")
+
+	}
 
 }
 
