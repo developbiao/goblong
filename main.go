7b68c2c1198968665a787447e45af168d61ea4c1
Gong Biao <gongbiao@Gongs-MacBook-Pro.local>
2021-09-02 23:01:47 +0800

    init goblog

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello, this is my goblog</h1>")
+
+}
+
+func main() {
+	http.HandleFunc("/", handleFunc)
+	http.ListenAndServe(":3000", nil)
+}
