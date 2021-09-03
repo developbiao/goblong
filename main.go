156747318f5f5fe66a2ffd4d8337142a15a96185
developbiao <developbiao@gmail.com>
2021-09-03 14:04:42 +0800

    try get using http.ServeMux

--- main.go
+++ main.go
@@ -9,19 +9,22 @@ func handleFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1 style=\"color:pink\">Hello, here is my goblog</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "<h1>This blog just for record leanring golang, if you have any quetion please contact "+
-			"<a href=\"mailto:developbiao@gmail.com\">developbiao@gmail.com</a></h1>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>Reqeust not found page :(</h1>"+
 			"<p>If you have any doubts, please contact us. </p>")
 
 	}
+}
 
+func aboutHandle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>This blog just for record leanring golang, if you have any quetion please contact "+
+		"<a href=\"mailto:developbiao@gmail.com\">developbiao@gmail.com</a></h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handleFunc)
+	http.HandleFunc("/about", aboutHandle)
 	http.ListenAndServe(":3000", nil)
 }
