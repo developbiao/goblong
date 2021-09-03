240b49de922eaee9081f90c50309f099f02c2a3c
developbiao <developbiao@gmail.com>
2021-09-03 11:17:58 +0800

    add auto load

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, here is my goblog</h1>")
+		fmt.Fprint(w, "<h1 style=\"color:pink\">Hello, here is my goblog</h1>")
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "<h1>This blog just for record leanring golang, if you have any quetion please contact "+
 			"<a href=\"mailto:developbiao@gmail.com\">developbiao@gmail.com</a></h1>")
