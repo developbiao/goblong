4b7741a47fc7d630be3800f89d9af6679bd6b471
developbiao <developbiao@gmail.com>
2021-09-14 23:16:44 +0800

    add test about page

--- tests/pages_test.go
+++ tests/pages_test.go
@@ -21,6 +21,21 @@ func TestHomePage(t *testing.T) {
 
 	// 2. Check status is 200
 	assert.NoError(t, err, "Has error happen, err is not empty")
-	assert.Equal(t, 200, resp.StatusCode, "Should be result status is 200")
+	assert.Equal(t, 200, resp.StatusCode, "Should be return status is 200")
 
+}
+
+func TestAboutPage(t *testing.T) {
+	baseURL := "http://localhsot:3000"
+
+	var (
+		resp *http.Response
+		err  error
+	)
+
+	resp, err = http.Get(baseURL + "/about")
+
+	// Assert check status
+	assert.NoError(t, err, "has error happen,  err is not empty")
+	assert.Equal(t, 200, resp.StatusCode, "Should be return status is  200")
 }
