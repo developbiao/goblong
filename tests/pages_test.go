d74aa6c8998b1fd4472f25a49dbff6edfff10214
developbiao <developbiao@gmail.com>
2021-09-14 23:29:42 +0800

    add test all page

--- tests/pages_test.go
+++ tests/pages_test.go
@@ -2,40 +2,51 @@ package tests
 
 import (
 	"net/http"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHomePage(t *testing.T) {
-	baseURL := "http://localhost:3000"
-
-	// Mock user request broswer
-
-	var (
-		resp *http.Response
-		err  error
-	)
-
-	resp, err = http.Get(baseURL + "/")
-
-	// 2. Check status is 200
-	assert.NoError(t, err, "Has error happen, err is not empty")
-	assert.Equal(t, 200, resp.StatusCode, "Should be return status is 200")
-
-}
-
-func TestAboutPage(t *testing.T) {
+func TestAllPage(t *testing.T) {
 	baseURL := "http://localhsot:3000"
 
-	var (
-		resp *http.Response
-		err  error
-	)
+	// Define all initilization test data
+	var tests = []struct {
+		method   string
+		url      string
+		expected int
+	}{
+		{"GET", "/", 200},
+		{"GET", "/about", 200},
+		{"GET", "/notfound", 404},
+		{"GET", "/articles", 200},
+		{"GET", "/articles/create", 200},
+		{"GET", "/articles/3", 200},
+		{"GET", "/articles/3/edit", 200},
+		{"POST", "/articles/3", 200},
+		{"POST", "/articles", 200},
+		{"POST", "/articles/1/delete", 404},
+	}
 
-	resp, err = http.Get(baseURL + "/about")
+	for _, test := range tests {
+		t.Logf("Curernt request URL: %v \n", test.url)
+		var (
+			resp *http.Response
+			err  error
+		)
 
-	// Assert check status
-	assert.NoError(t, err, "has error happen,  err is not empty")
-	assert.Equal(t, 200, resp.StatusCode, "Should be return status is  200")
+		switch {
+		case test.method == "POST":
+			data := make(map[string][]string)
+			resp, err = http.PostForm(baseURL+test.url, data)
+		default:
+			resp, err = http.Get(baseURL + test.url)
+		}
+
+		assert.NoError(t, err, "Request "+test.url+" report error")
+		assert.Equal(t, test.expected, resp.StatusCode, test.url+" should response status code"+strconv.Itoa(test.expected))
+
+	}
+
 }
